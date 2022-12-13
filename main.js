9c6b22a8033a29319dbe03e47ec00ffedb8ae75c
Frem <fremfremov@gmail.com>
2022-12-13 17:55:52 +0300

    master

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert(123)
+
+const TEST = {
+    test: 1,
+    test1() {
+        console.log('1')
+    }
+}
+const TEST1 = JSON.parse(JSON.stringify(TEST))
+TEST1.test2 = 2
+console.log(TEST)
+console.log(TEST1)
+
+
